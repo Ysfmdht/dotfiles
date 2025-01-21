3153045654da7225495a2e7038a336a48589d46b
Youssef Medhat <youssefmedhat@Youssefs-MacBook-Pro.local>
2025-01-21 02:40:01 +0200

    lsp changes

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -35,7 +35,6 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "jedi_language_server",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
