ba8a3dcddb48e45f07211ed26c2372db065236a8
Youssef Medhat <youssefmedhat@Youssefs-MacBook-Pro.local>
2025-02-02 06:33:19 +0200

    added term

--- .config/nvim/lua/joe/remap.lua
+++ .config/nvim/lua/joe/remap.lua
@@ -3,3 +3,10 @@ vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<space>st", function ()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 15)
+end)
